7c139e6d664e605ff0466d871814ea74a15bcb4c
ShpriZZ <Dzerenus@ya.ru>
2021-10-05 10:44:17 +0900

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SHPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string FileName 
+        {
+            set
+            {
+                fileName = value;
+                ToPlayerFrame();
+            }
+        }
+        string fileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FMain.Content = new StartFrame(this);
+        }
+
+        void ToPlayerFrame()
+        {
+            FMain.Content = new PlayerFrame(fileName);
+
+            var splits = fileName.Split('\\');
+            string mname = splits[splits.Length-1];
+
+            if (mname.Length > 40)
+                mname = mname.Substring(0, 38) + "...";
+
+            lHeader.Content = mname;
+        }
+
+        #region Нажатие и отпускание кнопок.
+
+        // Изменение цвета кнопок заголовка, когда курсор заходит на кнопку.
+        void btnMouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 1;
+        }
+
+        // Изменение цвета кнопок заголовка, когда курсор заходит на кнопку.
+        void btnMouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.85;
+        }
+
+        // Изменение цвета кнопок заголовка, когда курсор заходит на кнопку.
+        void btnMouseDown(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.6;
+        }
+
+        #endregion
+
+        #region События работы с заголовком формы.
+
+        void rHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        void iClose_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        void iHide_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) 
+        { 
+            WindowState = WindowState.Minimized; 
+        }
+
+        #endregion
+    }
+}
---
+++ StartFrame.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SHPlayer
+{
+    /// <summary>
+    /// Логика взаимодействия для StartFrame.xaml
+    /// </summary>
+    public partial class StartFrame : Page
+    {
+        DoubleAnimation hoverAnim;
+        DoubleAnimation leaveAnim;
+
+        MainWindow mWindow;
+
+        public StartFrame(MainWindow main)
+        {
+            mWindow = main;
+
+            hoverAnim = new DoubleAnimation();
+            hoverAnim.From = 1;
+            hoverAnim.To = 0.6;
+            hoverAnim.Duration = TimeSpan.FromMilliseconds(100);
+
+            leaveAnim = new DoubleAnimation();
+            leaveAnim.From = 0.6;
+            leaveAnim.To = 1;
+            leaveAnim.Duration = TimeSpan.FromMilliseconds(100);
+
+            InitializeComponent();
+        }
+
+        private void LeftMouseEnter(object sender, MouseEventArgs e)
+        {
+            hoverAnim.From = lChose.Opacity;
+
+            lChose.BeginAnimation(OpacityProperty, hoverAnim);
+            rChose.BeginAnimation(OpacityProperty, hoverAnim);
+        }
+
+        private void LeftMouseLeave(object sender, MouseEventArgs e)
+        {
+            leaveAnim.From = lChose.Opacity;
+
+            lChose.BeginAnimation(OpacityProperty, leaveAnim);
+            rChose.BeginAnimation(OpacityProperty, leaveAnim);
+        }
+
+        private void lChose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var opf = new OpenFileDialog();
+            opf.Filter = "MP3 File|*.mp3|WAV File|*.wav";
+            opf.Title = "Chose sound file";
+
+            bool? res = opf.ShowDialog();
+
+            if (res == true) mWindow.FileName = opf.FileName;
+        }
+    }
+}
